23db6944194fb7d03e1ad5dc42ad843ca3cb9cf8
Mehran <khazaeizadeh.m@gmail.com>
2025-02-07 23:26:37 +0330

    feat: add header file for command system library export/import definitions

---
+++ command-system/src/command_system_global.h
@@ -0,0 +1,13 @@
+#ifndef COMMAND_SYSTEM_LIBRARY_H
+#define COMMAND_SYSTEM_LIBRARY_H
+
+#include <QMetatype>
+#include <QtCore/qglobal.h>
+
+#if defined(COMMAND_SYSTEM_LIBRARY)
+#  define COMMAND_SYSTEM_LIBRARY Q_DECL_EXPORT
+#else
+#  define COMMAND_SYSTEM_LIBRARY Q_DECL_IMPORT
+#endif
+
+#endif // COMMAND_SYSTEM_LIBRARY_H
