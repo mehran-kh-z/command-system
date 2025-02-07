4f92302ac155d0e534acf969da76885559061e57
Mehran <khazaeizadeh.m@gmail.com>
2025-02-07 23:26:59 +0330

    feat: add CommandInterface class for command execution

---
+++ command-system/include/command_interface.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Evoteam {
+
+  class CommandInterface {
+  public:
+    virtual ~CommandInterface() = default;
+    virtual void execute() = 0;
+  };
+
+} // namespace Evoteam
